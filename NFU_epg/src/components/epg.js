f10a9fc5e0b5af79fa8ed129be3f53db6cdfda17
vicnx <andanivicente@gmail.com>
2020-12-21 17:00:16 +0100

    add day and dayName

--- NFU_epg/src/components/epg.js
+++ NFU_epg/src/components/epg.js
@@ -26,9 +26,30 @@ export default class Epgloader extends Component {
     componentWillUnmount() {
     //   clearInterval(this.timerID);
     }
+
+    getDayName(seconds){
+        // console.log(date.getDay());
+        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        var d = new Date(0);
+        d.setUTCSeconds(seconds); 
+        var dayName = days[d.getDay()];
+        console.log(dayName);
+        return dayName;
+    }
     
     secondstotime(seconds){
-        return new Date(seconds * 1000).toISOString().substr(11, 8)
+        if(seconds==null){
+            return ["test","test"]
+        }
+        let hours = this.convertUTCtoDate(seconds);
+        let hoursarray=hours.split(" ");
+        return hoursarray;
+    }
+
+    convertUTCtoDate(data){
+        let d=new Date(0);
+        d.setUTCSeconds(data);                          
+        return d.toLocaleString("es-ES",{});  
     }
 
     handleClick(start,duration){
@@ -39,21 +60,25 @@ export default class Epgloader extends Component {
         if(this.state.isLoaded){
             console.log(this.state.epg);
             const events_array = Object.values(this.state.epg.events);
-            console.log(this.secondstotime(events_array.slice(0)[0].spa.start));
-            console.log(this.secondstotime(events_array.slice(-1)[0].spa.start+events_array.slice(-1)[0].spa.duration));
+            let last_day;
 
 
             return (
                 <IonContent>
                     <IonRow>
                         <IonSlides options={this.options}>
-
-                            {events_array.map((event) => (
+                            
+                            {events_array.map((event,i,arr) => (
                                 <IonSlide style={{width:event.spa.duration/7}}  key={event.spa.id} onClick={ 
                                         () => this.handleClick(event.spa.start,event.spa.duration) 
                                     }> 
+                                    <IonRow className="timeline_day">
+                                        {/* {(this.secondstotime((arr[i-1]) ? arr[i-1].spa.start:null)[0]===this.secondstotime(event.spa.start)[0])?<span></span>:<span>{last_day===this.secondstotime(event.spa.start)[0]}{this.secondstotime(event.spa.start)[0]}</span>} */}
+                                        <span>{this.secondstotime(event.spa.start)[0]}  ({this.getDayName(event.spa.start)})</span>
+
+                                    </IonRow>
                                     <IonRow className="timeline">
-                                        <span>{this.secondstotime(event.spa.start)}</span>
+                                        {(arr.length -1 === i)?<span>{this.secondstotime(event.spa.start+event.spa.duration)[1]}</span>:<span>{this.secondstotime(event.spa.start)[1]}</span>}
                                     </IonRow>
                                     <IonCard>
                                         <IonCardHeader>
